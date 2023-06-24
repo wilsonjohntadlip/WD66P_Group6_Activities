01ce2e85c83ecbf4107f74919bf4f65965651408
Racel Amper <raamper01@gmail.com>
2023-06-24 13:31:27 +0800

    Activity 6

---
+++ Amper_Racel/Activity6/loginjava.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<script>
+function validate(){
+let emailadd = document.getElementById("emailadd").value;
+let pswrd = document.getElementById("pswrd").value;
+if ( emailadd == "Forget" && pswrd == "forget#123"){
+alert ("Login successfully");
+}
+</script>
+
+</body>
+</html>
