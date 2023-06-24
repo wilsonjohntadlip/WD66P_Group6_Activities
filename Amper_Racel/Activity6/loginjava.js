88ebd0cac44e3d599d89db40df5fd7878ec1a545
Racel Amper <raamper01@gmail.com>
2023-06-24 17:33:03 +0800

    activity 6 little progress

--- Amper_Racel/Activity6/loginjava.js
+++ Amper_Racel/Activity6/loginjava.js
@@ -3,12 +3,7 @@
 <body>
 
 <script>
-function validate(){
-let emailadd = document.getElementById("emailadd").value;
-let pswrd = document.getElementById("pswrd").value;
-if ( emailadd == "Forget" && pswrd == "forget#123"){
-alert ("Login successfully");
-}
+   
 </script>
 
 </body>
